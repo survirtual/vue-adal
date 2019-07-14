5f75eafc77c1945f6ebb099c02e8b1f477214454
Wes Hobbie <wesley.p.hobbie@accenture.com>
2019-07-14 18:09:39 -0500

    Cleanup

--- rollup.config.js
+++ rollup.config.js
@@ -16,30 +16,7 @@ export default {
     commonjs(),
     resolve(),
     babel({
-      exclude: 'node_modules/**',
-      babelrc: false,
-      'presets': [['@babel/preset-env', {
-        'modules': false,
-        'targets': [
-          'Chrome >= 52',
-          'FireFox >= 44',
-          'Safari >= 7',
-          'Explorer 11',
-          'last 4 Edge versions'
-        ]
-      }]
-      ],
-      'plugins': [
-        ['module-resolver', {
-          'root': ['./src'],
-          'alias': {
-            '@': './src/',
-            '@test': './test'
-          }
-        }]
-      ],
-      'sourceMaps': false,
-      'retainLines': false
+      exclude: 'node_modules/**'
     }),
     replace({
       ' const ': ' var ',
